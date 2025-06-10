297789ab3073fea6be40d787fa3ed56db6a2bab7
Анатолий Радченко <mrkotbest@gmail.com>
2025-06-10 12:01:20 +0300

    End of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -8,6 +8,7 @@ builder.Services.AddDbContext<DataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
     
 builder.Services.AddOpenApi();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -18,5 +19,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
+app.UseCors(policy =>
+    policy
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .WithOrigins("http://localhost:4200", "https://localhost:4200")
+    );
+
 app.MapControllers();
 app.Run();
